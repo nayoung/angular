f70a421870af7457e71ab71c9149170df9f9fa86
mia <gndl311@nate.com>
2018-05-26 11:04:44 +0900

    7장 라우팅구성

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,29 @@ import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {Route, RouterModule, Routes} from '@angular/router';
+
+// 부모컴포넌트에 router-outlet추가
+// 라우팅구성
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'detail/:id', component: HeroDetailComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
